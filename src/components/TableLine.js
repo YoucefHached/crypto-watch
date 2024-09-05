6f6c7001cbb53814dab20a4ca5d56bc7c7881787
YoucefHached <youha7105@gmail.com>
2024-09-05 20:34:46 +0100

    filter and favorites integration

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -1,4 +1,6 @@
 import React from "react";
+import PercentChange from "./PercentChange";
+import StarIcon from "./StarIcon";
 
 const TableLine = ({ coin, index }) => {
   const priceFormater = (num) => {
@@ -12,11 +14,14 @@ const TableLine = ({ coin, index }) => {
     }
   };
 
-  const mktCapFormater = (num) => {};
+  const mktCapFormater = (num) => {
+    let newNum = String(num).split("").slice(0, -6).join("");
+    return Number(newNum);
+  };
   return (
     <div className="table-line">
       <div className="infos-container">
-        <span>*</span>
+        <StarIcon coinId={coin.id} />
         <p>{index + 1}</p>
         <div className="img">
           <img src={coin.image} alt={"logo" + coin.symbol} height={20} />
@@ -40,6 +45,21 @@ const TableLine = ({ coin, index }) => {
         </div>
       </div>
       <p>{priceFormater(coin.current_price).toLocaleString() + " $ "}</p>
+      <p className="mktcap">
+        {mktCapFormater(coin.market_cap).toLocaleString() + " M$ "}
+      </p>
+      <p className="volume">{coin.total_volume.toLocaleString() + " $ "}</p>
+      <PercentChange percent={coin.price_change_percentage_1h_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_24h_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_7d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_30d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_200d_in_currency} />
+      <PercentChange percent={coin.price_change_percentage_1y_in_currency} />
+      {coin.ath_change_percentage > -3 ? (
+        <p>ATH !</p>
+      ) : (
+        <PercentChange percent={coin.ath_change_percentage} />
+      )}
     </div>
   );
 };
